4ee2df6e52dd14517cd40f6926af2c2059075d78
yudish <yudishchakrawarty3042@gmail.com>
2022-12-23 10:27:24 +0530

    yud

--- server.js
+++ server.js
@@ -10,12 +10,14 @@ import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
 import uploadRoutes from './routes/uploadRoutes.js'
-
+import cors from 'cors';
 dotenv.config()
 
 connectDB()
 
 const app = express()
+
+app.use(cors())
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
